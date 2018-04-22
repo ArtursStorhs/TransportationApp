7e63687cf1746d77f8c19bd597f995296887d964
Arturs Storhs <storhs.arturs@gmail.com>
2018-04-22 15:54:42 +0300

    connection to server instead of localhost

--- app/src/main/java/com/tsi/storhs/transportationapp/MainActivity.java
+++ app/src/main/java/com/tsi/storhs/transportationapp/MainActivity.java
@@ -1,7 +1,17 @@
 package com.tsi.storhs.transportationapp;
-import android.app.Activity;
-import android.os.Bundle;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +20,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        new MyAsyncTask().execute(true);
+    }
+
+    private class MyAsyncTask extends AsyncTask<Boolean, Boolean, Boolean> {
+
+        @Override protected Boolean doInBackground(Boolean... booleans) {
+            OkHttpClient client = new OkHttpClient.Builder().connectTimeout(45, TimeUnit.SECONDS).build();
+
+            HttpUrl build = new HttpUrl.Builder()
+                    .scheme("http")
+                    .host("192.168.1.102")
+                    .port(8889)
+                    .addPathSegment("mob")
+                    .build();
+            Request request = new Request.Builder().url(build).header("Some", "Shit").build();
+
+            try {
+                Response response = client.newCall(request).execute();
+            } catch (IOException e) {
+                Log.e("some", "shit", e);
+            }
+            return null;
+        }
     }
 }
